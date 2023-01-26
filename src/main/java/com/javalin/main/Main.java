e4cadcc8bde18255e11d50250f8c29d660269862
enzonun <enzomartnun@gmail.com>
2023-01-26 13:42:18 -0300

    ant run works without dependencies

--- src/main/java/com/javalin/main/Main.java
+++ src/main/java/com/javalin/main/Main.java
@@ -4,6 +4,7 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world java 11 with ant");
         var app = Javalin.create(/*config*/)
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
